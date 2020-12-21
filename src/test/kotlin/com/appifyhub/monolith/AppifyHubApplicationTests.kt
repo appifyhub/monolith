195ff18da2e13975fa9236ebd5dea05d73b60343
Milos Marinkovic <milosmns@gmail.com>
2020-12-21 22:14:24 +0100

    Add Spring Boot skeleton

---
+++ src/test/kotlin/com/appifyhub/monolith/AppifyHubApplicationTests.kt
@@ -0,0 +1,11 @@
+package com.appifyhub.monolith
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AppifyHubApplicationTests {
+
+  @Test fun `context loads`() = println("Context loaded")
+
+}
