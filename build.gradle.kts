3407f8c734d46a04341c6ecf3a1e3c9fa3791af9
Milos Marinkovic <milosmns@gmail.com>
2020-12-23 17:22:50 +0100

    Add publishing to GitHub releases

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,35 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.joda.time.Instant
+import org.joda.time.format.DateTimeFormat
+
+// region Build script configuration
+
+buildscript {
+  repositories {
+    jcenter()
+    mavenCentral()
+  }
+  dependencies {
+    classpath("joda-time:joda-time:+")
+  }
+}
 
 plugins {
-  id("org.springframework.boot") version "2.4.1"
-  id("io.spring.dependency-management") version "1.0.10.RELEASE"
   kotlin("jvm") version "1.4.21"
   kotlin("plugin.spring") version "1.4.21"
   kotlin("plugin.jpa") version "1.4.21"
+
+  id("org.springframework.boot") version "2.4.1"
+  id("io.spring.dependency-management") version "1.0.10.RELEASE"
+  id("com.github.breadmoirai.github-release") version "2.2.12"
 }
 
 repositories {
   jcenter()
   mavenCentral()
 }
-
-fun prop(name: String) = properties[name] ?: error("Missing '$name' in gradle.properties")
-
-group = prop("group")
-version = prop("version")
-val artifact = prop("artifact")
-
-java.sourceCompatibility = JavaVersion.VERSION_11
-java.targetCompatibility = JavaVersion.VERSION_11
 
 dependencies {
   implementation("org.jetbrains.kotlin:kotlin-reflect")
@@ -40,17 +49,98 @@ dependencies {
   runtimeOnly("org.postgresql:postgresql")
 }
 
-tasks.withType<Jar> {
-  archiveFileName.set("$artifact.jar")
-}
+// endregion
 
-tasks.withType<KotlinCompile> {
-  kotlinOptions {
-    freeCompilerArgs = listOf("-Xjsr305=strict")
-    jvmTarget = "11"
+// region Project configuration
+
+group = prop("group")
+version = prop("version")
+val artifact = prop("artifact")
+
+java.sourceCompatibility = JavaVersion.VERSION_11
+java.targetCompatibility = JavaVersion.VERSION_11
+
+// endregion
+
+// Tasks configuration
+
+tasks {
+
+  withType<Jar> {
+    archiveFileName.set("$artifact.jar")
   }
+
+  withType<KotlinCompile> {
+    kotlinOptions {
+      freeCompilerArgs = listOf("-Xjsr305=strict")
+      jvmTarget = "11"
+    }
+  }
+
+  withType<Test> {
+    useJUnitPlatform()
+    with(testLogging) {
+      showStandardStreams = true
+      exceptionFormat = TestExceptionFormat.FULL
+      events = setOf(TestLogEvent.PASSED, TestLogEvent.FAILED, TestLogEvent.SKIPPED)
+    }
+  }
+
+  named("githubRelease") {
+    dependsOn("bootJar")
+  }
+
 }
 
-tasks.withType<Test> {
-  useJUnitPlatform()
+// endregion
+
+// Plugin configuration
+
+githubRelease {
+  val writeToken = Env.get("GITHUB_TOKEN")
+  if (writeToken == Env.INVALID) println("Set 'PACKAGES_TOKEN' environment variable to enable GitHub releases")
+
+  val quality = Env.get("BUILD_QUALITY", default = "Debug")
+  println("GitHub release configured for '$quality' quality")
+
+  val commitish = Env.get("GITHUB_SHA", default = "main")
+
+  val tag = when (quality) {
+    "Debug" -> {
+      val formatter = DateTimeFormat.forPattern("yyyy_MM_dd_HH_mm_ss").withZoneUTC()
+      val timestamp = Instant.now().toString(formatter)
+      "v$version.${quality.toLowerCase()}.$timestamp"
+    }
+    else -> "v$version.${quality.toLowerCase()}"
+  }
+
+  token(writeToken)
+  owner(group.toString().split(".").last())
+  repo(artifact)
+  tagName(tag)
+  releaseName("Service [$quality]: $version")
+  targetCommitish(commitish)
+  prerelease(quality != "GA")
+  body("## Changelog\n* ${changelog().call().trim().split("\n").joinToString("\n* ")}")
+
+  releaseAssets(arrayOf(
+    file("${project.buildDir}/libs/$artifact.jar")
+  ))
 }
+
+apply(plugin = "com.github.breadmoirai.github-release")
+
+// endregion
+
+// Helpers
+
+object Env {
+  const val INVALID = "<invalid>"
+  fun get(name: String, default: String = INVALID) =
+    System.getenv(name).takeIf { !it.isNullOrBlank() } ?: default
+}
+
+fun prop(name: String) = properties[name]?.toString() // from gradle.properties
+  ?: error("Missing required '$name' in gradle.properties")
+
+// endregion
