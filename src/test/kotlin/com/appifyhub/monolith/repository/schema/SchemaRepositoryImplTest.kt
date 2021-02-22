8dd42f9c127b183769ce4ac7ef901b2ca7e8d8e0
Milos Marinkovic <milosmns@gmail.com>
2021-02-22 18:31:07 +0100

    Add SchemaRepository tests

---
+++ src/test/kotlin/com/appifyhub/monolith/repository/schema/SchemaRepositoryImplTest.kt
@@ -0,0 +1,115 @@
+package com.appifyhub.monolith.repository.schema
+
+import assertk.all
+import assertk.assertThat
+import assertk.assertions.hasClass
+import assertk.assertions.isFailure
+import assertk.assertions.isFalse
+import assertk.assertions.isSuccess
+import assertk.assertions.isTrue
+import assertk.assertions.messageContains
+import com.appifyhub.monolith.storage.dao.SchemaDao
+import com.appifyhub.monolith.storage.model.schema.SchemaDbm
+import com.appifyhub.monolith.util.Stubs
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.doAnswer
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.doThrow
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.stub
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.util.Optional
+
+class SchemaRepositoryImplTest {
+
+  private val schemaDao = mock<SchemaDao>()
+
+  private val repository: SchemaRepository = SchemaRepositoryImpl(
+    schemaDao = schemaDao,
+  )
+
+  @BeforeEach fun setup() {
+    schemaDao.stub {
+      onGeneric { save(any()) } doAnswer { it.arguments.first() as SchemaDbm }
+    }
+  }
+
+  // region Updates
+
+  @Test fun `updating schema with a previous schema initialized fails`() {
+    schemaDao.stub {
+      onGeneric { findById(Stubs.schema.version) } doReturn Optional.of(Stubs.schemaDbm)
+    }
+
+    assertThat { repository.update(Stubs.schema) }
+      .isFailure()
+      .all {
+        hasClass(IllegalArgumentException::class)
+        messageContains("already initialized")
+      }
+  }
+
+  @Test fun `updating schema with a previous schema not initialized works`() {
+    schemaDao.stub {
+      val schema = SchemaDbm(Stubs.schema.version, isInitialized = false)
+      onGeneric { findById(Stubs.schema.version) } doReturn Optional.of(schema)
+    }
+
+    assertThat { repository.update(Stubs.schema) }
+      .isSuccess()
+  }
+
+  @Test fun `updating schema with no previous schema works`() {
+    schemaDao.stub {
+      onGeneric { findById(Stubs.schema.version) } doReturn Optional.empty()
+    }
+
+    assertThat { repository.update(Stubs.schema) }
+      .isSuccess()
+  }
+
+  // endregion
+
+  // region Is initialized checks
+
+  @Test fun `fetching 'is initialized' returns false if DAO throws`() {
+    schemaDao.stub {
+      onGeneric { findById(Stubs.schema.version) } doThrow IllegalStateException("failed")
+    }
+
+    assertThat(repository.isInitialized(Stubs.schema.version))
+      .isFalse()
+  }
+
+  @Test fun `fetching 'is initialized' returns false if schema is missing`() {
+    schemaDao.stub {
+      onGeneric { findById(Stubs.schema.version) } doReturn Optional.empty()
+    }
+
+    assertThat(repository.isInitialized(Stubs.schema.version))
+      .isFalse()
+  }
+
+  @Test fun `fetching 'is initialized' returns false for non-initialized schemas`() {
+    schemaDao.stub {
+      val schema = SchemaDbm(Stubs.schema.version, isInitialized = false)
+      onGeneric { findById(Stubs.schema.version) } doReturn Optional.of(schema)
+    }
+
+    assertThat(repository.isInitialized(Stubs.schema.version))
+      .isFalse()
+  }
+
+  @Test fun `fetching 'is initialized' returns true for initialized schemas`() {
+    schemaDao.stub {
+      onGeneric { findById(Stubs.schema.version) } doReturn Optional.of(Stubs.schemaDbm)
+    }
+
+    assertThat(repository.isInitialized(Stubs.schema.version))
+      .isTrue()
+  }
+
+  // endregion
+
+}
