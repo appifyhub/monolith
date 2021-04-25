f0afdb6008d5887de5f8b41c8e1d60bb46eaa5e2
Milos Marinkovic <milosmns@gmail.com>
2021-04-25 12:08:52 +0200

    Add admin user controller test, cleanup

---
+++ src/test/kotlin/com/appifyhub/monolith/controller/admin/AdminUserControllerTest.kt
@@ -0,0 +1,136 @@
+package com.appifyhub.monolith.controller.admin
+
+import assertk.all
+import assertk.assertThat
+import assertk.assertions.isDataClassEqualTo
+import assertk.assertions.isEqualTo
+import com.appifyhub.monolith.TestAppifyHubApplication
+import com.appifyhub.monolith.controller.admin.AdminUserController.Endpoints.ANY_USER
+import com.appifyhub.monolith.domain.user.User.Authority.DEFAULT
+import com.appifyhub.monolith.domain.user.User.Authority.MODERATOR
+import com.appifyhub.monolith.network.common.MessageResponse
+import com.appifyhub.monolith.network.user.DateTimeMapper
+import com.appifyhub.monolith.network.user.UserResponse
+import com.appifyhub.monolith.util.AuthTestHelper
+import com.appifyhub.monolith.util.Stubs
+import com.appifyhub.monolith.util.TimeProviderFake
+import com.appifyhub.monolith.util.TimeProviderSystem
+import com.appifyhub.monolith.util.bearerAuthRequestEntity
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.client.exchange
+import org.springframework.boot.web.server.LocalServerPort
+import org.springframework.http.HttpMethod
+import org.springframework.http.HttpStatus
+import org.springframework.test.annotation.DirtiesContext
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.junit.jupiter.SpringExtension
+
+@ExtendWith(SpringExtension::class)
+@ActiveProfiles(TestAppifyHubApplication.PROFILE)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@SpringBootTest(
+  classes = [TestAppifyHubApplication::class],
+  webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+)
+class AdminUserControllerTest {
+
+  @Autowired lateinit var timeProvider: TimeProviderFake
+  @Autowired lateinit var restTemplate: TestRestTemplate
+  @Autowired lateinit var authTestHelper: AuthTestHelper
+
+  @LocalServerPort var port: Int = 0
+  private val baseUrl: String by lazy { "http://localhost:$port" }
+
+  @BeforeEach fun setup() {
+    timeProvider.staticTime = { TimeProviderSystem().currentMillis }
+  }
+
+  @AfterEach fun teardown() {
+    timeProvider.staticTime = { null }
+  }
+
+  @Test fun `get any user fails when unauthorized`() {
+    val userId = authTestHelper.defaultUser.userId
+
+    assertThat(
+      restTemplate.exchange<MessageResponse>(
+        url = "$baseUrl$ANY_USER",
+        method = HttpMethod.GET,
+        requestEntity = bearerAuthRequestEntity("invalid"),
+        uriVariables = mapOf(
+          "projectId" to userId.projectId,
+          "id" to userId.id,
+        ),
+      )
+    ).all {
+      transform { it.statusCode }.isEqualTo(HttpStatus.UNAUTHORIZED)
+    }
+  }
+
+  @Test fun `get any user succeeds for self`() {
+    val user = authTestHelper.defaultUser
+    val token = authTestHelper.newRealToken(DEFAULT).token.tokenValue
+
+    assertThat(
+      restTemplate.exchange<UserResponse>(
+        url = "$baseUrl$ANY_USER",
+        method = HttpMethod.GET,
+        requestEntity = bearerAuthRequestEntity(token),
+        uriVariables = mapOf(
+          "projectId" to user.userId.projectId,
+          "id" to user.userId.id,
+        ),
+      )
+    ).all {
+      transform { it.statusCode }.isEqualTo(HttpStatus.OK)
+      transform { it.body!! }.isDataClassEqualTo(
+        Stubs.userResponse.copy(
+          userId = user.userId.id,
+          universalId = user.userId.toUniversalFormat(),
+          type = user.type.name,
+          authority = user.authority.name,
+          birthday = DateTimeMapper.formatAsDate(user.birthday!!),
+          createdAt = DateTimeMapper.formatAsDateTime(timeProvider.currentDate),
+          updatedAt = DateTimeMapper.formatAsDateTime(timeProvider.currentDate),
+        )
+      )
+    }
+  }
+
+  @Test fun `get any user succeeds for lower rank`() {
+    val user = authTestHelper.defaultUser
+    val token = authTestHelper.newRealToken(MODERATOR).token.tokenValue
+
+    assertThat(
+      restTemplate.exchange<UserResponse>(
+        url = "$baseUrl$ANY_USER",
+        method = HttpMethod.GET,
+        requestEntity = bearerAuthRequestEntity(token),
+        uriVariables = mapOf(
+          "projectId" to user.userId.projectId,
+          "id" to user.userId.id,
+        ),
+      )
+    ).all {
+      transform { it.statusCode }.isEqualTo(HttpStatus.OK)
+      transform { it.body!! }.isDataClassEqualTo(
+        Stubs.userResponse.copy(
+          userId = user.userId.id,
+          universalId = user.userId.toUniversalFormat(),
+          type = user.type.name,
+          authority = user.authority.name,
+          birthday = DateTimeMapper.formatAsDate(user.birthday!!),
+          createdAt = DateTimeMapper.formatAsDateTime(timeProvider.currentDate),
+          updatedAt = DateTimeMapper.formatAsDateTime(timeProvider.currentDate),
+        )
+      )
+    }
+  }
+
+}
