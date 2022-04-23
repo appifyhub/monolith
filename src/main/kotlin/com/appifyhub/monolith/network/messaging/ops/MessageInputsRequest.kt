f479bce9daeecbfcaf114a10aa9a557933d65f35
Milos Marinkovic <milosmns@gmail.com>
2022-04-23 17:27:41 +0200

    Add message templating controller, cleanup

--- src/main/kotlin/com/appifyhub/monolith/network/messaging/ops/MessageInputsRequest.kt
+++ src/main/kotlin/com/appifyhub/monolith/network/messaging/ops/MessageInputsRequest.kt
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonInclude.Include
 import com.fasterxml.jackson.annotation.JsonProperty
 import java.io.Serializable
 
+// additional access verification is done in the controller
 @JsonInclude(Include.NON_NULL)
 data class MessageInputsRequest(
   @JsonProperty("user_id") val universalUserId: String? = null,
