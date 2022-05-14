e01a0771dee78148c49958ee04c95e344b1ff5b5
Milos Marinkovic <milosmns@gmail.com>
2022-05-14 13:07:20 +0200

    Clean up access manager

--- src/main/kotlin/com/appifyhub/monolith/service/access/AccessManagerImpl.kt
+++ src/main/kotlin/com/appifyhub/monolith/service/access/AccessManagerImpl.kt
@@ -7,8 +7,6 @@ import com.appifyhub.monolith.domain.user.User
 import com.appifyhub.monolith.domain.user.UserId
 import com.appifyhub.monolith.service.access.AccessManager.Feature
 import com.appifyhub.monolith.service.access.AccessManager.Feature.BASIC
-import com.appifyhub.monolith.service.access.AccessManager.Feature.DEMO
-import com.appifyhub.monolith.service.access.AccessManager.Feature.MESSAGE_TEMPLATES
 import com.appifyhub.monolith.service.access.AccessManager.Feature.USERS
 import com.appifyhub.monolith.service.access.AccessManager.Privilege
 import com.appifyhub.monolith.service.auth.AuthService
@@ -208,8 +206,6 @@ class AccessManagerImpl(
         when (feature) {
           BASIC -> true // always supported
           USERS -> true // always supported
-          MESSAGE_TEMPLATES -> true // always supported
-          DEMO -> false // unsupported for now
         }
       }
 
