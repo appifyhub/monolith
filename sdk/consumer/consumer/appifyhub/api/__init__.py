7e3df022e46634580613fe6984c7cb3998d1dce4
GitHub Action <action@github.com>
2024-03-30 19:20:32 +0000

    Update SDK docs [skip ci]

---
+++ sdk/consumer/consumer/appifyhub/api/__init__.py
@@ -0,0 +1,8 @@
+# flake8: noqa
+
+# import apis into api package
+from appifyhub.api.auth_api import AuthApi
+from appifyhub.api.health_api import HealthApi
+from appifyhub.api.messaging_api import MessagingApi
+from appifyhub.api.user_api import UserApi
+
