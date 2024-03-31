eadeaebacf82f048df0db66c24f51adc1d838bdd
GitHub Action <action@github.com>
2024-03-31 17:51:33 +0200

    Clean up SDK docs

--- sdk/creator/appifyhub/api/__init__.py
+++
@@ -1,8 +0,0 @@
-# flake8: noqa
-
-# import apis into api package
-from appifyhub.api.auth_api import AuthApi
-from appifyhub.api.messaging_api import MessagingApi
-from appifyhub.api.projects_api import ProjectsApi
-from appifyhub.api.users_api import UsersApi
-
